c980ca3f7345f679c65c050d8362c1c006299101
klabarre <klabarre@dawan.fr>
2025-04-18 09:35:25 +0200

    correction pagination pokedex

--- src/pages/pokedex/pokedex.component.ts
+++ src/pages/pokedex/pokedex.component.ts
@@ -16,8 +16,9 @@ export class PokedexComponent implements OnInit {
   // Attribut pour solution 1
   pokemons: IResumePokemon[] = []
 
-  // Attribut pour solution 1
-  fullPokemons: IPokemon[] = []
+  // Attributs pour solution 2
+  // fullPokemons: IPokemon[] = []
+  responseApi: { pokemons: IPokemon[], count: number } = { pokemons: [], count: 0 }
 
   limit = 20;
   offset = 0;
@@ -39,8 +40,35 @@ export class PokedexComponent implements OnInit {
   // SOLUTION 2
   loadFullPokemon() {
     this.service.getPokemons(this.limit, this.offset).subscribe({
-      next: (response) => this.fullPokemons = response.pokemons
+      next: (response) => {
+        this.responseApi = response
+        console.log("RESPONSE -> ", this.responseApi)
+      }
     })
+  }
+
+  nextPage() {
+    this.offset += this.limit
+    this.loadFullPokemon()
+    this.loadPokemon()
+  }
+
+  previousPage() {
+    this.offset -= this.limit
+    if (this.offset < 0) this.offset = 0
+    this.loadFullPokemon()
+    this.loadPokemon()
+  }
+
+  get pagniationInfo(): { currentPage: number, lastPage: number } {
+    // Math.floor : fonction qui renvoie le plus grand entier inférieur ou égal à un nombre.
+    const currentPage = Math.floor(this.offset / this.limit) + 1
+    // Math.ceil() : fonction qui renvoie le plus petit entier supérieur ou égal à un nombre.
+    const lastPage = Math.ceil(this.responseApi.count / this.limit)
+
+
+    // return { currentPage: currentPage, lastPage: lastPage } // Equivalent de la ligne suivante
+    return { currentPage, lastPage }
   }
 
 
